d554a9a0195262a8667dde06cfcca29d93a24961
IshiiRyotaro <work070719ryotaro@gmail.com>
2024-01-09 19:37:30 +0900

    コメント追加

--- study01/lib/main.dart
+++ study01/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-
+// コメントを入れています
+// チーム開発ありがとうございました
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
